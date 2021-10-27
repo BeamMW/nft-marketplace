c2036c9ef0d94deb9c31a1a395471a7dd9235aa0
chapati <chapati.th@gmail.com>
2021-10-27 16:03:27 +0300

    styles

--- html/main.js
+++ html/main.js
@@ -3,6 +3,7 @@ import {store}  from './store.js'
 import {router} from './router.js'
 import utils    from './utils/utils.js'
 
+// TODO: ensure we create 6.1 API
 utils.initialize({
     "appname": "BEAM Gallery",
     "apiResultHandler": (...args) => store.onApiResult(...args)
@@ -12,7 +13,13 @@ utils.initialize({
     vueApp.config.globalProperties.$state = store.state
     vueApp.use(router)
     vueApp.mount('body')
-    
+
+    const style = document.createElement('style');
+    style.innerHTML = `.error {color: ${utils.getStyles().validator_error};}`
+    document.head.appendChild(style)
+    document.body.style.color = utils.getStyles().content_main
+    store.start()
+
     if (err) {
         return store.setError(err)
     }
