3f22f4208adc6a0ed8ddebf04edab2c1c2511ea0
Georges Markosian <georgesmarkosian@gmail.com>
2022-03-17 20:03:19 +0400

    Refactor button

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,65 +1,156 @@
 <template>
-  <button 
-    class="button" 
-    :class="[transparency, color, side_icon, disabled_button]" 
-    :disabled="disabled"
+  <button class="button"
+          :class="[transparency, color, side_icon, disabled_button]"
+          :disabled="disabled"
   >
-    <img 
-      v-if="icon" 
-      :src="icon" 
-    />
+    <img v-if="icon" :src="icon"/>
     <span class="text" :style="color_text">{{ text }}</span>
   </button>
 </template>
 
+<style scoped lang="stylus">
+$magenta = #da68f5;
+$blue = #0bccf7;
+$green = #00f6d2;
+$semiTransparent = rgba(255, 255, 255, 0.1);
+$darkBlue = #032e49;
+$white = #ffffff;
+$marginIcon = 8px;
+
+.button {
+  height: 38px;
+  padding: 11px 25px;
+  border-radius: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  color: $darkBlue;
+  font-size: 14px;
+  font-weight: bold;
+  cursor: pointer;
+  border: none;
+
+  &:not(.disabled):hover {
+    box-shadow: 0 0 8px white;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  & .text {
+    line-height: 1;
+  }
+}
+
+.disabled {
+  opacity: 0.3;
+  cursor: auto !important;
+}
+
+.transparent {
+  height: min-content;
+  background: transparent;
+  padding: 0;
+
+  &:not(.disabled):hover {
+    box-shadow: none;
+  }
+}
+
+.semiTransparent {
+  background: $semiTransparent;
+}
+
+.leadingIcon {
+  img {
+    margin-right: $marginIcon;
+  }
+}
+
+.trailingIcon {
+  flex-direction: row-reverse;
+
+  img {
+    margin-left: $marginIcon;
+  }
+}
+
+.singleIcon {
+  img {
+    margin: 0;
+  }
+}
+
+.magenta {
+  background-color: $magenta;
+}
+
+.blue {
+  background-color: $blue;
+}
+
+.green {
+  background-color: $green;
+}
+
+.black {
+  background-color: $black;
+  color: #fff;
+}
+</style>
+
 <script>
 export default {
-  data() {
-    return {
-      //
-      text_colors: {
-        magenta: "#da68f5",
-        blue: "#0bccf7",
-        green: "#00f6d2",
-        semiTransparent: "rgba(255, 255, 255, 0.1)",
-        darkBlue: "#032e49",
-        white: "#ffffff"
-      }
-    }
-  },
-
   props: {
     transparent: {
       type: Boolean,
-      default: false
+      default: false,
     },
     semi_transparent: {
       type: Boolean,
-      default: false
+      default: false,
     },
     // magenta, blue, green, semiTransparent, darkBlue
     color: {
       type: String,
+      default: String,
     },
     // icon source
     icon: {
-      type: String,  
+      type: String,
+      default: String,
     },
     // left, right
     icon_position: {
       type: String,
-      default: 'left'
+      default: 'left',
     },
     text: {
       type: String,
+      default: String,
     },
     // available colors from data.text_colors
     text_color: {
       type: String,
+      default: String,
     },
     disabled: {
       type: Boolean,
-      default: false
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      text_colors: {
+        magenta: '#da68f5',
+        blue: '#0bccf7',
+        green: '#00f6d2',
+        semi_transparent: 'rgba(255, 255, 255, 0.1)',
+        dark_blue: '#032e49',
+        white: '#ffffff',
+      },
     }
   },
 
@@ -83,112 +174,26 @@ export default {
       if (this.semi_transparent) {
         return 'semiTransparent'
       }
+
+      return ''
     },
     color_text() {
       if (this.text && this.text_color) {
         return {
-          "color": this.text_colors[this.text_color]
+          color: this.text_colors[this.text_color],
         }
       }
+
+      return ''
     },
     disabled_button() {
       if (this.disabled) {
         return 'disabled'
       }
-    }
-  }
+
+      return ''
+    },
+  },
 }
 </script>
 
-<style scoped lang="stylus">
-$magenta = #da68f5
-$blue = #0bccf7
-$green = #00f6d2
-$semiTransparent = rgba(255, 255, 255, 0.1)
-$darkBlue = #032e49
-$white = #ffffff
-
-$marginIcon = 8px
-
-.button {
-  height: 38px
-  padding: 11px 25px
-  border-radius: 50px
-  display: flex
-  flex-wrap: wrap
-  align-items: center
-  color: $darkBlue
-  font-size: 14px
-  font-weight: bold
-  cursor: pointer
-  border: none
-
-  &:not(.disabled):hover {
-    box-shadow: 0 0 8px white
-  }
-
-  &:focus {
-    outline: none
-  }  
-
-  & .text {
-    line-height: 1
-  }
-}
-
-.disabled {
-  opacity: 0.3;
-  cursor: auto !important;
-}
-
-.transparent {
-  height: min-content
-  background: transparent
-  padding: 0
-
-  &:not(.disabled):hover {
-    box-shadow: none
-  }
-}
-
-.semiTransparent {
-  background: $semiTransparent
-}
-
-.leadingIcon {
-  img {
-    margin-right: $marginIcon
-  }
-}
-
-.trailingIcon {
-  flex-direction: row-reverse
-  
-  img {
-    margin-left: $marginIcon
-  }
-}
-
-.singleIcon {
-  img {
-    margin: 0
-  }
-}
-
-.magenta {
-  background-color: $magenta
-}
-
-.blue {
-  background-color: $blue
-}
-
-.green {
-  background-color: $green
-}
-
-.black {
-  background-color: $black
-  color: #fff
-}
-</style>
