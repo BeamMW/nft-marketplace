c46044eb91ffbc042f940eb531df38237170dae0
Georges Markosian <georgesmarkosian@gmail.coma>
2022-04-03 02:25:05 +0400

    Refactor formatter

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -11,7 +11,7 @@ export default function formatter(num) {
 
   if (fixedNum >= 1000 && fixedNum < 1000000) {
     const head = fixedNum.slice(0, -7)
-    const tail = fixedNum.slice(-6, -4)
+    const tail = fixedNum.slice(-7, -5)
     const n = +(head + '.' + tail)
 
     return n + ' k'
@@ -19,7 +19,7 @@ export default function formatter(num) {
 
   if (fixedNum >= 1000000 && fixedNum < 1000000000) {
     const head = fixedNum.slice(0, -10)
-    const tail = fixedNum.slice(-8, -6)
+    const tail = fixedNum.slice(-10, -8)
     const n = +(head + '.' + tail)
 
     return n + ' m'
@@ -27,7 +27,7 @@ export default function formatter(num) {
 
   if (fixedNum >= 1000000000) {
     const head = fixedNum.slice(0, -13)
-    const tail = fixedNum.slice(-10, -8)
+    const tail = fixedNum.slice(-13, -11)
     const n = +(head + '.' + tail)
 
     return n + ' b'
