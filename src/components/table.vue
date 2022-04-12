5fc06ceea79a294fcf5f3f28671cbb84dfbeb5c9
Georges Markosian <georgesmarkosian@gmail.coma>
2022-04-12 17:34:45 +0400

    Create table component

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,45 +1,27 @@
 <template>
-  <hdr title=""/>
   <!-- title to props -->
-  <h1 class="title">nft traiding history</h1> 
-  <table class="table">
-    <thead>
-      <tr>
-        <th v-for="(header, cellIdx) in headers" :key="cellIdx + header">
-          <img src="~assets/icon-beam.svg">
-          <span>{{ header_cell(header, cellIdx, 1) }}</span>
-          <!-- <span>{{ header }}</span> -->
-          <img src="~assets/sort-arrows.svg">
-        </th>
-      </tr>
-    </thead>
+  <h1 class="table__title">{{ title }}</h1> 
+  <div class="table" :style="table_style">
+    <div v-for="cell in rows" 
+         :key="cell.value" 
+         :class="cell.isHeaderCell ? 'header' : 'cell'"
+         :style="cellStyle(cell)"
+    >
+      <span v-if="cell.isImage">
+        <img class="icon" src="~assets/icon-beam.svg">
+        {{ cell.value }}
+      </span>
+      <span v-else :class="{ 'bold': cell.isBold }">{{ cell.value }}</span>
 
-    <tbody>
-      <tr v-for="(item, rowIdx) in mock_data" :key="rowIdx + item">
-        <td v-for="(value, cellIdx) in item" :key="rowIdx + cellIdx + value">
-          <span>{{ value }}</span>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+      <img v-if="cell.isHeaderCell" class="arrows" src="~assets/sort-arrows.svg">
+    </div>
+  </div>
 </template>
 
 <style lang="stylus" scoped>
-table
-tbody
-thead
-tr
-th
-td {
-  border: 0
-}
-
-table {
-  border-spacing: 0
-}
-
-.title {
-  font-family: 'SFProDisplay', sans-serif
+.table__title {
+  width: 100%
+  font-family: 'SF Pro Display', sans-serif
   font-weight: 700
   font-size: 14px
   line-height: 17px
@@ -51,11 +33,161 @@ table {
 }
 
 .table {
-  width: 100%
   border-radius: 10px 10px 0 0
   font-size: 14px
-  font-family: 'SFProDisplay', sans-serif
+  font-weight: 100
+  line-height: 24px
+  font-family: 'SF Pro Display', sans-serif
   overflow: hidden
+
+  & span {
+    display: flex
+    align-items: center
+  }
+
+  & .arrows {
+    margin-left: 8px
+  }
+  
+  & .icon {
+    margin-right: 8px
+  }
+
+  & .amount {
+    font-weight: 100
+  }
+
+  & .bold {
+    font-weight: bold
+  }
+}
+
+.header {
+  height: 46px
+  display: flex
+  align-items: center
+  padding-left: 20px
+  color: rgba(255, 255, 255, 0.5)
+
+  &:last-child {
+    padding-right: 20px
+  }
+}
+
+.cell {
+  height: 56px
+  display: flex
+  align-items: center
+  padding-left: 20px
+
+  &:last-child {
+    padding-right: 20px
+  }
+}
+</style>
+
+<script>
+// import amount from './amount.vue'
+
+export default {
+  components: {
+    // amount
+  },
+  
+  props: {
+    title: {
+      type: String,
+      default: undefined
+    },
+    columns: {
+      type: Array,
+      default: undefined,
+      required: true
+    },
+    dataset: {
+      type: Array,
+      default: undefined,
+      required: true
+    },
+    image_idx: {
+      type: Number,
+      default: null
+    },
+    bold_idx: {
+      type: Number,
+      default: null
+    },
+    ellepsis_idx: {
+      type: Number,
+      default: null
+    }
+  },
+
+  computed: {
+    rows() {
+      const headerCells = this.columns.map(column => ({value: column.name, isHeaderCell: true}))
+      const bodyCells = this.dataset
+        .map((item, rowIdx) => {
+          const row = this.columns.map(column => ({
+            value: item[column.id] || 'n/a',
+            isHeaderCell: false,
+            isEvenRow: rowIdx % 2 === 0,
+            isImage: false,
+            isBold: false
+          }))
+
+          // render cell
+          if (this.image_idx !== null) {
+            row[this.image_idx].isImage = true
+          }
+
+          if (this.bold_idx !== null) {
+            row[this.bold_idx].isBold = true
+          }
+
+          if (this.ellepsis_idx !== null) {
+            const el = row[this.ellepsis_idx].value
+
+            if (el.length <= 12) return row
+            
+            const head = el.slice(0, 6)
+            const tail = el.slice(el.length - 6, el.length)
+            
+            row[this.ellepsis_idx].value = `${head}...${tail}`
+          }
+
+          return row
+        })
+        .flat()
+        
+      // console.log(headerCells, bodyCells)
+      return [...headerCells, ...bodyCells]
+    },
+
+    table_style() {
+      return `display: grid; grid-template-columns: ${this.columns.map(() => 'auto').join(' ')}`
+    },
+  },
+  
+  methods: {
+    cellStyle(cell) {
+      if (cell.isHeaderCell) {
+        return 'background-color: rgba(255, 255, 255, 0.08)'
+      }
+
+      if (cell.isEvenRow) {
+        return 'background-color: rgba(255, 255, 255, 0.01)'
+      }
+
+      return 'background-color: rgba(255, 255, 255, 0.05)'
+    }
+  }
+}
+</script>
+
+  & tr {
+    max-height: 57px
+  }
   
   & tr:nth-child(odd) {
     background-color: rgba(255, 255, 255, 0.05)
@@ -77,7 +209,7 @@ table {
     background-color: rgba(255, 255, 255, 0.08)
     white-space: nowrap
 
-    img {
+    .ardataset {
       margin-left: 5px
     }
   }
@@ -89,54 +221,12 @@ table {
   & td {
     padding-right: 20px
     text-align: left
-  }
-}
-</style>
 
-<script>
-import hdr from './page-title.vue'
+    border: 1px solid cyan
 
-import DATA from '../utils/mock-data.js'
-
-export default {
-  components: {
-    hdr,
-  },
-  
-  data() {
-    return {
-      headers: ['Author', 'Book', 'Year']
-    }
-  },
-  // props: {
-  //   columns: {
-  //     type: Array,
-  //     default: undefined,
-  //     required: true
-  //   },
-  //   rows: {
-  //     type: Array,
-  //     default: undefined,
-  //     required: true
-  //   }
-  // }
-
-  computed: {
-    mock_data() {
-      return DATA
-    }
-  },
-
-  methods: {
-    header_cell(value, idx, param) {
-      if (idx === param) {
-        return 'TEST'
-      }
-
-      return value
+    & img {
+      margin: -20px
     }
   }
 
-}
-</script>
-
+         :style="cell.isEvenRow && !cell.isHeaderCell ? 'background-color: cyan' : 'background-color: magenta'"
--- src/utils/mock-data.js
+++ src/utils/mock-data.js
@@ -1,8 +1,7 @@
 const data = [
-  ['Lewis Carroll', 'Alice In Wonderland', '1865'],
-  ['Frederic Bastiat', 'Ce qu\'on voit et ce qu\'on ne voit pas.', '1850'],
-  ['Ludwig von Mises', 'Soliaslism', '1922'],
-  ['Friedrich August von Hayek', 'The Road to Serfdom', '1944']
+  {id: 1, author: 'Lewis Carroll', book: 'Alice In Wonderland', year: '1865', long: '1232453'},
+  {id: 2, author: 'Frederic Bastiat', book: 'Ce qu\'on voit et ce qu\'on ne voit pas.', year: '1850', long: '87098932451923847981728736498'},
+  {id: 3, author: 'Friedrich August von Hayek', book: 'The Road to Selfdom', year: '1944', long: '9879809891237419287873148724981628'}
 ]
 
 export default data
