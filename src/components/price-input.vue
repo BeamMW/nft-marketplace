c4c9caf36857b1d387e308fc37e7c2df7fb761f0
chapati <chapati.th@gmail.com>
2022-04-14 17:26:37 +0300

    gallery wip

--- src/components/price-input.vue
+++ src/components/price-input.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="price-input-container">
+  <div :style="{'--color': color}">
     <div class="input">
       <input id="sell-input" 
              :value="price"
@@ -12,60 +12,56 @@
       />
       <span class="text">BEAM</span>
     </div>
-    <!-- TODO: add USD support 
-    <span class="price">{{ price }} USD</span>  
-    -->
+    <span class="price">{{ price }} USD</span>
   </div>
 </template>
 
 <style scoped lang="stylus">
-.price-input-container {
-  .input {
-    margin-top: 20px
-    height: 45px
-    width: 100%
+.input {
+  margin-top: 20px
+  height: 45px
+  width: 100%
+  border-radius: 10px
+  background-color: rgba(255, 255, 255, 0.05)
+  display: flex
+  flex-direction: row
+  align-items: center
+
+  .elem {
+    background: rgba(255, 255, 255, 0)
+    border: none
+    font-size: 24px
+    resize: none
+    color: var(--color)
+    height: 100%
+    padding: 0
     border-radius: 10px
-    background-color: rgba(255, 255, 255, 0.08)
-    display: flex
-    flex-direction: row
-    align-items: center
+    padding: 8px 8px 8px 20px
+    max-width: 292px
 
-    .elem {
-      background: transparent
-      border: none
-      font-size: 24px
-      resize: none
+    &:focus {
+      outline-width: 0
       color: var(--color)
-      height: 100%
-      padding: 0
-      border-radius: 10px
-      padding: 12px 15px
-      max-width: 292px
-
-      &:not(.err):not(:read-only):focus {
-        background-color: rgba(255, 255, 255, 0.12)
-        outline: none
-      }
-
-      &::placeholder {
-        color: var(--color)
-      }
     }
 
-    .text {
-      margin-right: 12px
-      font-size: 16px
-      margin-left: auto
+    &::placeholder {
       color: var(--color)
     }
   }
 
-  .price {
-    display: block
-    margin-top: 6px
-    font-size: 12px
+  .text {
+    margin-right: 12px
+    font-size: 16px
+    margin-left: auto
     color: var(--color)
   }
+}
+
+.price {
+  display: block
+  margin-top: 6px
+  font-size: 12px
+  color: var(--color)
 }
 </style>
 
