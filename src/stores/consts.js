180afe4584927a18482b587b36d4743fdcece065
chapati <chapati.th@gmail.com>
2022-07-21 21:06:43 +0300

    Revert "ownership detection fixes"
    This reverts commit 4315abcfdd6706d85c2c38ad343eae1b0a5c496e.

--- src/stores/consts.js
+++ src/stores/consts.js
@@ -1,7 +1,7 @@
 import {contract} from 'utils/consts'
 
 export const versions = {
-  DATABASE_VERSION:   121,
+  DATABASE_VERSION:   120,
   ARTIST_VERSION:     200,
   COLLECTION_VERSION: 200,
   NFT_VERSION:        200,
